a6f2670881f4b21fc7ff8d17aac2cd312958eea0
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-09-12 16:54:51 +0300

    разбит на компоненты код

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react'
+import { useState } from "react";
+import "./App.css";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      Class Component Works
-      </div>
-    )
-  }
-}
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
 
-export default App
+  const deleteTodo = (text) => {
+const NewTodos = todos.filter((todo) =>{
+  return todo !== text;
+});
+setTodos(NewTodos);
+  };
+
+
+
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>React Todo App</h1>
+      <TodoInput todo= {todo} setTodo = {setTodo} addTodo = {addTodo} />
+      <TodoList list={todos} remove = {deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
